5666662468f4ac58e965be4e49e5b4ca8b4888cf
Jon Pye <jdpye@dal.ca>
2026-06-06 22:10:29 -0300

    fixing up the metadata for publishing to PyPI

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyIPT',
-    version='0.1.0',
+    version='0.1.4',
     author='Jon Pye',
     author_email='jonpye@gmail.com',
     description='Python tools for publishing data to a GBIF IPT instance',
