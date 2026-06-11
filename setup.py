ace0cda5f694dc55746562adec6921d23d105ab9
Jon Pye <jdpye@dal.ca>
2026-06-11 06:59:28 -0300

    expanding python versions compatibility

--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='pyIPT',
-    version='0.1.4',
+    version='0.1.5',
     author='Jon Pye',
     author_email='jonpye@gmail.com',
     description='Python tools for publishing data to a GBIF IPT instance',
